ae6037b2eaa54392aa463749c0374c0ab12f4865
Jingyi Niu <elonniu@amazon.com>
2023-08-06 19:47:53 +0800

    improved task show

--- packages/functions/index.js
+++ packages/functions/index.js
@@ -346,7 +346,7 @@ async function versionCheck() {
 }
 
 function show(data) {
-    const colWidths = [20, 40, 20, 40];
+    const colWidths = [20, 50, 20, 50];
 
     const table = new Table({
         head: [chalk.yellow('Key1'), chalk.green('Value1'), chalk.yellow('Key2'), chalk.green('Value2')],
